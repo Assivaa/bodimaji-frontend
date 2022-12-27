7c5344f7d6ebac60a53d4f7e9e55f47bd9d89c42
reyvidod <reyvidod@gmail.com>
2022-12-28 00:01:14 +0700

    react router on app.jsx, implement article page

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import ArticlePage from "../../components/article/Article";
 import BeforeFooter from "../../components/beforeFooter/BeforeFooter";
 import Footer from "../../components/footer/Footer";
 
-const About = () => {
+const Article = () => {
   return (
     <>
       <Navbar />
+      <ArticlePage />
       <BeforeFooter />
       <Footer />
     </>
   );
 };
 
-export default About;
+export default Article;
