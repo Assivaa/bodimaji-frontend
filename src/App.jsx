52bf9644f9060492af54a61e5edb71d8fdac7553
reyvido <reyvidod@gmail.com>
2023-01-02 14:18:39 +0700

    scroll to top for react router

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,39 @@ import Article from "./pages/article/Article.jsx";
 import ProductPage from "./pages/product/Product";
 import Register from "./pages/register/Register.jsx";
 import AllCollection from "./pages/collections/Collections.jsx";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="/about">
-          <Route index element={<About />} />
-        </Route>
-        <Route path="/news">
-          <Route index element={<News />} />
-        </Route>
-        <Route path="/article">
-          <Route index element={<Article />} />
-        </Route>
-        <Route path="/articles">
-          <Route index element={<AllArticle />} />
-        </Route>
-        <Route path="/collection">
-          <Route index element={<AllCollection />} />
-        </Route>
-        <Route path="/product">
-          <Route index element={<ProductPage />} />
-        </Route>
-        <Route path="/register">
-          <Route index element={<Register />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="/about">
+            <Route index element={<About />} />
+          </Route>
+          <Route path="/news">
+            <Route index element={<News />} />
+          </Route>
+          <Route path="/article">
+            <Route index element={<Article />} />
+          </Route>
+          <Route path="/articles">
+            <Route index element={<AllArticle />} />
+          </Route>
+          <Route path="/collection">
+            <Route index element={<AllCollection />} />
+          </Route>
+          <Route path="/product">
+            <Route index element={<ProductPage />} />
+          </Route>
+          <Route path="/register">
+            <Route index element={<Register />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
