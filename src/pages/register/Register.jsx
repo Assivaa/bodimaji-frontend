ff674cff0ddbd43ca4c3822b5953d7e36d07366d
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2023-01-02 21:37:37 +0700

    import css

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import BeforeFooter from "../../components/beforeFooter/BeforeFooter";
 import Footer from "../../components/footer/Footer";
+import "./login.css";
 
 const Register = () => {
   return (
