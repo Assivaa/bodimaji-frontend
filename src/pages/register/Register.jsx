de617e3dd788b3e33a423201ac71480246654507
reyvido <reyvidod@gmail.com>
2023-01-03 08:50:51 +0700

    fix register page styling

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,48 +4,48 @@ import BeforeFooter from "../../components/beforeFooter/BeforeFooter";
 import Footer from "../../components/footer/Footer";
 import "./register.css";
 
+import socialGoogle from "../../resources/icons/icon-google.svg";
+import socialFacebook from "../../resources/icons/icon-facebook.svg";
+
 const Register = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <div className="registers-registers">
-            <div className="kotak">
-          <div className="registers-form">
-            <div className="registers-createaccount">
-              Create Account
-              <div className="registers-xxxaspartofbodimaji ">
-                xxxx as part of bodimaji
-                <div className="registers-Rectangle1">
-                  <input type="text"></input>
-                </div>
-                <div className="registers-Rectangle1">
-                  <input type="text" placeholder="Fullname"></input>
-                </div>
-                <div className="registers-Rectangle1">
-                  <input type="text" placeholder="Email Address"></input>
-                </div>
-                <div className="registers-Rectangle1">
-                  <input type="password" placeholder="Password"></input>
-                </div>
-                <div>Minimum 8 characters</div>
-                <div className="registers-Rectangle1">
-                  <input type="password" placeholder="Confirm Password"></input>
-                </div>
-                <div className="registers-term">
-                  <input type="checkbox" /> I agree to Terms & Conditions
-                </div>
-                <div>
-                  <button className="registers-Rectangle2">
-                    <div className="registers-Register"><p>Register</p></div>
-                  </button>
-                </div>
-              </div>
+      <div className="register-body">
+        <div className="register-wrapper">
+          <div className="register-form-header">
+            <span>Create Account</span>
+            <p> Join as part of bodimaji</p>
+          </div>
+          <div className="register-form-input">
+            <input type="text" placeholder="Fullname"></input>
+            <input type="text" placeholder="Email Address"></input>
+            <input type="password" placeholder="Password"></input>
+            <input type="password" placeholder="Confirm Password"></input>
+          </div>
+          <div className="register-form-term">
+            <input type="checkbox" /> <span>I agree to Terms & Conditions</span>
+          </div>
+          <div className="register-form-button">
+            <button className="register-button">
+              <span>Register</span>
+            </button>
+          </div>
+          <div className="register-social-container">
+            <div className="register-social-header">or sign up with</div>
+            <div className="register-social-button">
+              <img src={socialGoogle} alt="google" />
+              <img src={socialFacebook} alt="facebook" />
             </div>
+            <span>Already have an account?</span>
           </div>
+          <div className="register-form-button">
+            <button className="login-button">
+              <span>Login</span>
+            </button>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
       <BeforeFooter />
       <Footer />
     </>
