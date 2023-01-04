b9642594c7438ad777088e591f5e2149f6c781d6
asiva syhrm <assiva.syahrani9e@gmail.com>
2023-01-04 22:01:25 +0700

    update dashboard admin add sidebar in dashboard

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -3,20 +3,24 @@ import NavbarAdmin from "../../components/navbaradmin/Admin";
 import { FaProductHunt } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import "./dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
       <NavbarAdmin />
-      <div className="dashboardpage">
-
+      <div className="dashboardpage-white">
+      <div className="dashboardpage"></div>
       {/* sidebar */}
         <ul class="side-menu top">
 			<li class="active">
+
+      <div className="dash-home">
         <a href="#">
               <FaHome /> <span class="text">Dashboard</span>
 				</a>
+        </div>
 			</li>
 			<li>
 				
@@ -42,51 +46,24 @@ const Dashboard = () => {
     {/* setting and logout */}
     <ul class="side-menu">
 			<li>
+      <div className="setting">
 				<a href="#">
-					<i class='bx bxs-cog' ></i>
-					<span class="text">Settings</span>
+					<FaArrowRight /><span class="text">Settings</span>
 				</a>
+        </div>
 			</li>
 			<li>
+      <div className="keluar">
 				<a href="#" class="logout">
-					<i class='bx bxs-log-out-circle' ></i>
-					<span class="text">Logout</span>
+        <FaArrowRight /><span class="text">Logout</span>
 				</a>
+      </div>
 			</li>
 		</ul>
-
-    {/* navbar */}
-
-    <div className="content">
-          <nav>
-            <i class='bx bx-menu' ></i>
-            <a href="#" class="nav-link">Categories</a>
-            <form action="#">
-              <div class="form-input">
-                <input type="search" placeholder="Search..."></input>
-                <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
-              </div>
-            </form>
-            <input type="checkbox" id="switch-mode" hidden></input>
-            <label for="switch-mode" class="switch-mode"></label>
-            <a href="#" class="notification">
-              <i class='bx bxs-bell' ></i>
-              <span class="num">8</span>
-            </a>
-            <a href="#" class="profile">
-              <img src="img/people.png"></img>
-            </a>
-          </nav>
-
-          
-
-    </div>
-    
+  
          
 
-
-
-      </div>
+       </div>
 
     </>
   );
