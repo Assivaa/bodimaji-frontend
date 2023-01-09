ca7c7d1bd293d35c71711c0480b4c43f9d2ca721
asiva syhrm <assiva.syahrani9e@gmail.com>
2023-01-09 23:22:37 +0700

    update forn and css in dashboard article

--- src/pages/dashboardarticle/DashboardArticle.jsx
+++ src/pages/dashboardarticle/DashboardArticle.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Dashboard from "../admin/dashboard";
 import { Link } from "react-router-dom";
 
+import "./dashboardarticle.css";
+
 const DashboardArticle = () => {
        return (
          <>
@@ -10,7 +12,18 @@ const DashboardArticle = () => {
           <div>
             <Link to="/article">
                 <span class="text">Article</span>
-             </Link>
+            </Link>
+          </div>
+          <div className="formarticle-input">
+              <input
+                type="text"
+                placeholder="Title"
+                required
+              ></input>
+              <input
+                type="text"
+                placeholder="Description"
+              ></input>
             </div>
         </div>
          </>
