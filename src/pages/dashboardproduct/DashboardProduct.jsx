91feb8fa8b12d624f9a8a53958a2ffb78c4516e2
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2023-01-10 16:17:49 +0700

    tambah dashboard product

--- src/pages/dashboardproduct/DashboardProduct.jsx
+++ src/pages/dashboardproduct/DashboardProduct.jsx
@@ -1,27 +1,51 @@
 import React from "react";
 import Dashboard from "../admin/dashboard";
+import { Link } from "react-router-dom";
 
 const DashboardProduct = () => {
   return (
     <>
-      <Dashboard />
-      <div className="sideright">
-        <div className="admin-search-product-box-container">
-          <h1>Search box</h1>
-          <p><input type="text" placeholder="Search product here" /></p>
-          <button type="button">Search</button>
-        </div>
+       <Dashboard />
+          <div className="sideright">
+              <div className="title-artcle">Owner</div>
+          <div className="formarticle-input">
+              <input
+                type="text"
+                placeholder="Owner"
+                required
+              ></input>
+              <div className="title-artcle2">Product Name</div>
+              <input
+                type="text"
+                placeholder="Product Name"
+              ></input>
+               <div className="title-artcle3">Description</div>
+              <input
+                type="text"
+                placeholder="Product Description"
+              ></input>
+              <div className="title-artcle3">Price</div>
+              <input
+                type="text"
+                placeholder="Product Price in Rp"
+              ></input>
+               <div className="title-artcle4">Upload Image</div>
+              <input
+                type="text"
+                placeholder="Upload Image URL"
+              ></input>
+            </div>
 
-        <div className="admin-add-product-button">
-          <button type="button">Add Product</button>
+            <div className="FormButton Submit">
+              <a className="Submitform"> 
+              <Link to="/admin/dashboard" className="LinkNav">
+                <button> 
+                  <span>Submit</span>
+                </button>
+                </Link>
+              </a>
+            </div>       
         </div>
-        <div className="admin-delete-product-button">
-          <button type="button">Delete Product</button>
-        </div>
-        <div className="admin-update-product-button">
-          <button type="button">Update Product</button>
-        </div>
-      </div >
     </>
   );
 };
