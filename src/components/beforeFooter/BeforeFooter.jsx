cdb0879ac331fc299a41909baac09730aac737a7
asiva syhrm <assiva.syahrani9e@gmail.com>
2023-01-10 22:11:30 +0700

    update icon before footer

--- src/components/beforeFooter/BeforeFooter.jsx
+++ src/components/beforeFooter/BeforeFooter.jsx
@@ -6,15 +6,36 @@ const BeforeFooter = () => {
     <div className="before-footer">
       <div className="features-container">
         <div className="features-list">
-          <div className="circle"></div>
+          <div className="circle">
+          <div className="circleimage">
+            <img
+              src={require("../../resources/images/baju.png")}
+              alt="baju-alt"
+            />
+          </div>
+          </div>
           <div className="features">Size-inclusive</div>
         </div>
         <div className="features-list">
-          <div className="circle"></div>
+          <div className="circle">
+          <div className="circleimage2">
+            <img
+              src={require("../../resources/images/love.png")}
+              alt="love-icon"
+            />
+          </div>
+          </div>
           <div className="features">Made to last</div>
         </div>
         <div className="features-list">
-          <div className="circle"></div>
+          <div className="circle">
+          <div className="circleimage3">
+            <img
+              src={require("../../resources/images/Package.png")}
+              alt="love-icon"
+            />
+          </div>
+          </div>
           <div className="features">Small batches</div>
         </div>
       </div>
