e623eb0da3f15fa653f38400692f62ee0acd7c46
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2023-01-10 16:45:01 +0700

    nambah dashboard product

--- src/pages/dashboardproduct/DashboardProduct.jsx
+++ src/pages/dashboardproduct/DashboardProduct.jsx
@@ -1,49 +1,50 @@
 import React from "react";
 import Dashboard from "../admin/dashboard";
 import { Link } from "react-router-dom";
+import "./dashboardproduct.css";
 
 const DashboardProduct = () => {
   return (
     <>
        <Dashboard />
           <div className="sideright">
-              <div className="title-artcle">Owner</div>
-          <div className="formarticle-input">
+              <div className="title-product">Owner</div>
+          <div className="formproduct-input">
               <input
                 type="text"
                 placeholder="Owner"
                 required
               ></input>
-              <div className="title-artcle2">Product Name</div>
+              <div className="title-product2">Product Name</div>
               <input
                 type="text"
                 placeholder="Product Name"
               ></input>
-               <div className="title-artcle3">Description</div>
+               <div className="title-product3">Description</div>
               <input
                 type="text"
                 placeholder="Product Description"
               ></input>
-              <div className="title-artcle3">Price</div>
+              <div className="title-product4">Price</div>
               <input
                 type="text"
                 placeholder="Product Price in Rp"
               ></input>
-               <div className="title-artcle4">Upload Image</div>
+               <div className="title-product5">Upload Image</div>
               <input
                 type="text"
                 placeholder="Upload Image URL"
               ></input>
             </div>
-
+            <br />
             <div className="FormButton Submit">
-              <a className="Submitform"> 
+              <div className="Submitform"> 
               <Link to="/admin/dashboard" className="LinkNav">
                 <button> 
                   <span>Submit</span>
                 </button>
                 </Link>
-              </a>
+              </div>
             </div>       
         </div>
     </>
